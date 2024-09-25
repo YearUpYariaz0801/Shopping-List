c4db9ace4438cc6da192284f52ae38811d494c5b
Yariaz0801 <designsbyzairay@gmail.com>
2024-09-25 15:47:15 -0400

    My Completed ShoppingList app

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args)
+    {
+        System.out.println("-----------------");
+
+        System.out.println("Apples");
+        System.out.println("Peanut Butter");
+        System.out.println("Coconuts");
+        System.out.println("Cashews");
+        System.out.println("Chicken");
+        System.out.println("Steak");
+        System.out.println("Turkey Wings");
+        System.out.println("Pie");
+        System.out.println("Soda");
+        System.out.println("Peaches");
+    }
+}
